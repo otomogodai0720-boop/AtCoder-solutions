951e92c56b53c0d558671a13a5025534e050a4ac
otomogodai0720-boop <otomo.godai0720@gmail.com>
2026-03-20 15:56:11 +0900

    Create A - 居合を終え、青い絵を覆う.cpp

---
+++ ABC/ABC_049/A - 居合を終え、青い絵を覆う.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string c;
+  cin >> c;
+  
+  if (c=="a" || c=="e" || c=="i" || c=="o" || c=="u") {
+    cout << "vowel" << endl;
+  }
+  
+  else {
+    cout << "consonant" << endl;
+  }
+  return 0;
+}
