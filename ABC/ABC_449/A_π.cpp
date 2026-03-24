a96b6c2d1dc779ca975e51171c57d5d910730503
otomogodai0720-boop <otomo.godai0720@gmail.com>
2026-03-24 23:56:19 +0900

    Update A_π.cpp

--- ABC/ABC_449/A_π.cpp
+++ ABC/ABC_449/A_π.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <cmath>
-#include <cstdio> // printfを使うため
-
+#include <iomanip>
 using namespace std;
 
-int main() {
-    double d;
-    if (!(cin >> d)) return 0;
+int main(){
+    int D;
+    cin >> D;
 
-    // 半径を正確に計算し、M_PI（または acos(-1.0)）を掛ける
-    double r = d / 2.0;
-    double ans = r * r * M_PI;
-
-    // 小数点以下15桁まで正確に出力する（これが一番確実！）
-    printf("%.15f\n", ans);
-    
+    cout << fixed << setprecision(1000) << M_PI*D*D/4 << endl;
     return 0;
 }
