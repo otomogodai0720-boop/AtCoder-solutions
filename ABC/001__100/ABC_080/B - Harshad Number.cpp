e56c80c83dac5230bc869fb06f9bfd3de87c4921
otomogodai0720-boop <otomo.godai0720@gmail.com>
2026-03-29 14:32:44 +0900

    Create B - Harshad Number.cpp

---
+++ ABC/001__100/ABC_080/B - Harshad Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  
+  int original_N = N;
+  int sum = 0;
+  
+  while (N>0) {
+    sum += N%10;  //一の位からどんどん足していく
+    N /=10;
+  }
+  
+  if (original_N % sum == 0) {
+    cout << "Yes" << endl;
+  }
+  
+  else {
+    cout << "No" << endl;
+  }
+}
