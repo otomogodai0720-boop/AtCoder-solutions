62c74efbc21defbe28a9df2dfca049b9ecb4337e
otomogodai0720-boop <otomo.godai0720@gmail.com>
2026-03-29 14:32:22 +0900

    Create A-Addition_and_Subtraction_Easy.cpp

---
+++ ABC/001__100/ABC_050/A-Addition_and_Subtraction_Easy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int A, B;
+  string op;
+  cin >> A >> op >> B;
+  
+  if (op == "+") {
+    cout << A+B << endl;
+  }
+  else {
+    cout << A-B << endl;
+  }
+  return 0;
+}
