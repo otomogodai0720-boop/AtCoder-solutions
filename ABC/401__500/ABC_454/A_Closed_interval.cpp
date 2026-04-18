c054c520753d6e8b46b03c86b4b0d887d85a6e18
otomogodai0720-boop <otomo.godai0720@gmail.com>
2026-04-18 21:57:00 +0900

    Create A_Closed_interval.cpp

---
+++ ABC/401__500/ABC_454/A_Closed_interval.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+
+int main() {
+    int l, r;
+    cin >> l >> r;
+    cout << r-l+1 << endl;
+    return 0;
+}
